1f8255d605569b3afdf48ec41b16bdbd3698c65e
nadagmal <nadagamalsobhy@gmail.com>
2025-04-28 17:01:27 +0300

    adding tailwind in the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,14 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">مرحباً بك في FilterTask!</h1>
+      </div>
+      
+
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind!</h1>
+
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // تأكد من أن المسار يشير إلى الملفات الصحيحة
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
