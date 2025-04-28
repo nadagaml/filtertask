211145a3de465365b178657576177e6ea0829ff8
nadagmal <nadagamalsobhy@gmail.com>
2025-04-28 22:43:51 +0300

    adding new file testymonials.jsx

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Home from './Component/Home';
 import Services from './Component/Services';
 
-
+import testymonials from './Component/testymonials';
 
 function App() {
   return (
     <div className="App">
       <Home/>
       <Services/>
+     <testymonials/>
     </div>
   );
 }
